6ad5c86c4223ac479beed2c63344e8f5e6778f60
pgr <greisch@contens.de>
2018-04-19 11:04:25 +0200

    aaqaaa

--- server/server.js
+++ server/server.js
@@ -34,6 +34,14 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(418).send(e);
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Started');
